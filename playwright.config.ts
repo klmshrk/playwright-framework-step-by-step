0f897df78c9af3e12dfbb19e3f0d3a82844dbef3
Klymentii <klimentiishark@gmail.com>
2025-08-11 15:37:34 +0300

    Add environment variable management and example configuration
    - Update .gitignore to include environment files
    - Create .env.example for environment variable setup
    - Load environment variables in Playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+// Determine which environment file to load
+const environmentPath = process.env.ENVIRONMENT 
+    ? `./env/.env.${process.env.ENVIRONMENT}`
+    : `./env/.env.dev`; // Default to dev environment
+
+// Load the environment variables
+dotenv.config({
+    path: environmentPath,
+});
+
 
 /**
  * See https://playwright.dev/docs/test-configuration.
